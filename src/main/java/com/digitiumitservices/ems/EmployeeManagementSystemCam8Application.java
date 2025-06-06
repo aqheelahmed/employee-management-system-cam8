68e0c0fdcdc6afc2af12f85cb6a6d642ce1e6fe8
aqheelahmed <aqheelahmed2003@gmail.com>
2025-06-06 15:07:21 +0530

    Resolved merge conflict in main class

--- src/main/java/com/digitiumitservices/ems/EmployeeManagementSystemCam8Application.java
+++ src/main/java/com/digitiumitservices/ems/EmployeeManagementSystemCam8Application.java
@@ -8,6 +8,7 @@ public class EmployeeManagementSystemCam8Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeManagementSystemCam8Application.class, args);
+		//making a conflict
 	}
 
 }
